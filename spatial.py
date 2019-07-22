dfb317b468a47cd30410dfc36fc8697d8cdb5cfa
JoshTa <3eternus@gmail.com>
2019-07-22 18:43:09 +0100

    Add files via upload

---
+++ spatial.py
@@ -0,0 +1,368 @@
+import axelrod as axl
+from axelrod.action import Action
+from axelrod.player import Player
+import neat
+import visualize
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+import os
+os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
+
+
+## Load the configuration file for NEAT library
+
+CONFIG_FILE = 'my-config.ini'
+config = neat.Config(
+    genome_type = neat.DefaultGenome,
+    reproduction_type = neat.DefaultReproduction, 
+    species_set_type = neat.DefaultSpeciesSet,
+    stagnation_type = neat.DefaultStagnation,
+    filename = CONFIG_FILE
+    )
+
+
+PLOT_ON_CONSOLE = True
+SAVE_PLOTS      = True 
+
+## How many matches each agent play with each other at one generation
+TOURNAMENT_TURNS = 3
+
+
+# How many generations should the code run
+MAX_GENERATIONS = 5
+
+
+# Integer encoding of Cooperation and Defect actions
+INT_DEFECT = -1
+INT_COOPERATION = 1
+
+
+# Read input size of network to use it in the code, do not change.
+NEAT_INPUT_SIZE = config.genome_config.num_inputs ## 2
+
+# It should be an even number since it will take last N action of both of the 
+#   agents and total input size will be 2 * N
+assert NEAT_INPUT_SIZE % 2 == 0, 'Input size should not be an odd number.'
+
+
+
+###############################################################################
+############### FUNCTIONS AND CLASSES TO INTEGRATE TWO LIBRARIES. #############
+####################### DON'T CHANGE ##########################################
+###############################################################################
+
+def action_to_binary(action):
+    '''
+    
+    Conversion from Axelrod action object to -1 or 1
+    
+    '''
+    if action == Action.C:
+        return INT_COOPERATION
+    elif action == Action.D:
+        return INT_DEFECT
+    else:
+        raise Exception('Invalid action type')
+
+
+
+
+def create_graph_edges(num_players, probability_of_edge):
+    '''
+    
+    Create graph edges for spatial tournament
+    
+    '''
+    assert probability_of_edge <= 1 and probability_of_edge >= 0
+    edges = []
+    played_matches = [0] * num_players
+    for row in range(num_players - 1):
+        edges.append((row, row + 1))
+        played_matches[row] += 1
+        played_matches[row + 1] += 1
+        for column in range(row, num_players):
+            if np.random.uniform() < probability_of_edge and column > row + 1:
+                edges.append((row, column))
+                played_matches[row] += 1
+                played_matches[column] += 1
+    return (played_matches, edges)
+
+        
+def net_output_to_action(net_output):
+    '''
+    
+    Conversion from neural network output to Axelrod action object
+    
+    '''
+    if net_output > 0:
+        return Action.D
+    else:
+        return Action.C
+    
+    
+    
+
+def create_node_names(input_size):
+    '''
+    
+    Create node names for visualization, regarding the input size of network
+    This works for 2, 4, 6 and 8 inputs.
+    
+    '''
+    
+    if input_size == 2:
+        node_names = {-1: 'My last action', 
+                      -2: 'Opponent\'s last action', 
+                       0: 'My decision'}
+    
+    elif input_size == 4:
+        node_names = {-1: 'My last action', 
+                      -2: 'My second last action', 
+                      -3: 'Opponent\'s last action', 
+                      -4: 'Opponent\'s second last action', 
+                       0: 'My decision'}
+    
+    elif input_size == 6:
+        node_names = {-1: 'My last action', 
+                      -2: 'My second last action', 
+                      -3: 'My third last action', 
+                      -4: 'Opponent\'s last action', 
+                      -5: 'Opponent\'s second last actionn', 
+                      -6: 'Opponent\'s third last action', 
+                       0: 'My decision'}
+    
+    elif input_size == 8:
+        node_names = {-1: 'My last action', 
+                      -2: 'My second last action', 
+                      -3: 'My third last action', 
+                      -4: 'My fourth last action', 
+                      -5: 'Opponent\'s last actionn', 
+                      -6: 'Opponent\'s second last action',
+                      -7: 'Opponent\'s third last action',
+                      -8: 'Opponent\'s fourth last action',
+                       0: 'My decision'}
+    return node_names
+
+
+
+
+class NeatAgent(Player):
+    
+    '''
+    
+    Class utility for encapsulating a NEAT neural net as Axelrod agent
+    
+    '''
+    
+    name = "NeatAgent"
+    
+    def __init__(self, neural_net):
+        super().__init__()
+        self.neural_net = neural_net
+
+
+
+
+    def strategy(self, opponent):
+        '''
+        
+        Overridden strategy function, this must be implemented for Axelrod custom
+        agents to work.
+        
+        '''
+        opponent_history_binary = list(map(action_to_binary, opponent.history))
+        self_history_binary = list(map(action_to_binary, self.history))
+
+        
+        if len(opponent_history_binary) < NEAT_INPUT_SIZE / 2:
+            opponent_history_binary = [INT_DEFECT] * NEAT_INPUT_SIZE
+            self_history_binary = [INT_DEFECT] * NEAT_INPUT_SIZE
+        
+        single_history_size = NEAT_INPUT_SIZE // 2
+        net_input = opponent_history_binary[-single_history_size:] \
+        + self_history_binary[-single_history_size:]
+        output = self.neural_net.activate(net_input)[0]
+        action = net_output_to_action(output)
+        return action
+    
+    
+    
+    def __repr__(self):
+        
+        ''' 
+        
+        This is just for prettier console output if you need to print it.
+        
+        '''
+        
+        return 'NEAT agent'
+        
+        
+    
+def create_players_from_genomes(genomes, config):
+    
+    '''
+    
+    Takes a population of NEAT genomes, makes it a player pool for 
+    Axelrod tournament. 
+    
+    '''
+    
+    agents = []
+    for _, genome in genomes:
+        neural_net = neat.nn.FeedForwardNetwork.create(genome, config)
+        agent = NeatAgent(neural_net)
+        agents.append(agent)
+    return agents
+    
+
+
+def cooperation_ratio(result_set):
+    
+    '''
+    
+    Calculates ratio of cooperation actions to the all actions in a tournament
+    
+    '''
+
+    cooperations = result_set.cooperating_rating
+    return np.mean(cooperations)
+
+
+def evaluate_genomes(genomes, config):
+    
+    '''
+   
+    Assign fitness value to each genome as its score in the tournament.
+    This must be implemented for NEAT library to work.
+    It also saves results of each tournament to a global variable to summarize 
+    and plot the results later
+    
+    '''
+    
+    global results_per_generation
+    global matches_played_per_player_per_generation
+
+    agents = create_players_from_genomes(genomes, config)
+    played_matches, edges  = create_graph_edges(len(genomes), probability_of_edge=0.5)
+    matches_played_per_player_per_generation.append(played_matches)
+    
+    tournament = axl.Tournament(agents, turns = TOURNAMENT_TURNS, edges=edges)
+    results = tournament.play()
+    results_per_generation.append(results)
+
+    mean_scores = np.mean(results.scores, axis = 1)
+    for n in range(len(genomes)):
+        genome = genomes[n][1]
+        genome.fitness = mean_scores[n]
+    
+###############################################################################
+###########  END OF THE FUNCTIONS AND THE CLASSES   ###########################
+#############  TO INTEGRATE TWO LIBRARIES.  ###################################
+###############################################################################
+###############################################################################
+
+
+  
+    
+'''
+    Create an empty list to store the tournament results for each generation
+'''
+results_per_generation = []       
+matches_played_per_player_per_generation = []   
+
+'''
+    Create the population, which is the top-level object for a NEAT run.
+'''
+p = neat.Population(config)
+
+'''
+    Create a stats object to use for showing fitness per generation graph.
+'''
+stats = neat.StatisticsReporter()
+p.add_reporter(stats)
+
+'''
+    If you also want outputs on console, uncomment this.
+'''
+#p.add_reporter(neat.StdOutReporter(True))
+
+
+'''
+    Run the NEAT algorithm for MAX_GENERATIONS time and select the best performing
+    agent from the last generation
+'''
+best_individual = p.run(evaluate_genomes, MAX_GENERATIONS)
+
+'''
+    Extract the neural net from genotype of best_indiviual
+'''
+best_net = neat.nn.FeedForwardNetwork.create(best_individual, config)
+
+
+'''
+    Visualizing the network topology
+'''
+node_names = create_node_names(NEAT_INPUT_SIZE)
+visualize.draw_net(config, best_individual, view = True, node_names=node_names)
+visualize.plot_stats(stats, ylog=False, view=True)
+
+
+'''
+    Plotting the cooperation ratio per generation
+'''
+cooperation_ratio_per_generation = [cooperation_ratio(results) for results in results_per_generation]
+plt.plot(cooperation_ratio_per_generation)
+plt.xlabel('Generations')
+plt.ylabel('Average cooperation ratio')
+plt.show()
+
+
+'''
+    Summarize each generation's tournament results and save it to a Excel document
+    in the summaries folder. It deletes the old summaries inside the folder first.
+    
+    Note: Do not delete summaries folder, this saves results in it and complains
+     if it can't find the folder.
+'''
+import os, glob
+files = glob.glob('summaries/*')
+for f in files:
+    os.remove(f)
+for n, results in enumerate(results_per_generation):
+    results.write_summary('summaries/summary' + str(n) + '.csv')
+
+
+'''
+    Summarize each generation's tournament results and save it in the 
+     plots folder if SAVE_PLOTS is True. It also shows it on the console if 
+     PLOT_ON_CONSOLE is True. It deletes the old plots inside 
+     the folder before saving.
+    
+    Note: Do not delete plots folder, this saves results in it and complains
+     if it can't find the folder.
+'''
+if SAVE_PLOTS:
+        files = glob.glob('plots/*')
+        for f in files:
+            os.remove(f)
+    
+for n, results in enumerate(results_per_generation):
+    wins = np.array([summary.Wins for summary in results.summarise()])
+    matches_played = np.array(matches_played_per_player_per_generation[n])
+    
+    win_ratio = wins / matches_played
+    coop_ratio = [summary.Cooperation_rating for summary in results.summarise()]
+    
+    plt.plot(win_ratio, 'ro')
+    plt.plot(coop_ratio, 'b^')
+    plt.xlabel('n_th Individual')
+    plt.ylabel('Win ratio & Cooperation ratio')    
+    plt.legend(('Win ratio', 'Cooperation ratio'))
+    if SAVE_PLOTS:
+        plt.savefig('plots/tournament' + str(n) + '.png')
+    if PLOT_ON_CONSOLE:
+        plt.show()
+
